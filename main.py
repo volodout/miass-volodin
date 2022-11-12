1ea614bc39eeb6da5843b766167ea161351affae
volodin <slavavolodin868@yandex.ru>
2022-11-12 18:42:48 +0500

    заготовка

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+
+from PyQt5.QtWidgets import QWidget, QApplication
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
